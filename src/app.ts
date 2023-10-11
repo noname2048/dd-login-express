d83ab5c68eec89a87d20fd96570306c602904e8c
noname2048 <sungwook.csw@gmail.com>
2023-10-11 10:33:05 +0900

    feat: create mock api

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 
 const app = express();
-const port = 5000;
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Typessciprt + Node.js + Express");
 });
 
-app.get("/user/login", (req: Request, res: Response) => {
-  const { username, password } = req.body;
+app.post("/user/login", (req: Request, res: Response) => {
+  console.log(req.body);
+  const { username, password }: { username: string; password: string } =
+    req.body;
+  if (username.length > 1 && username === password) {
+    res.status(200).json({ accessToken: "" }).send();
+  }
+  res.status(401).send();
 });
 
+const port = 5001;
 app.listen(port, () => {
   console.log(`[server] Example app listening at http://localhost:${port}`);
 });
