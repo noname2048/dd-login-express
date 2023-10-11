6a53f7726157d65dbd26a1fec601a126092a1aed
noname2048 <sungwook.csw@gmail.com>
2023-10-11 12:23:25 +0900

    feat: apply cookies

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,45 @@
 import express, { Express, Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
+const secretKey = "secretKey";
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Typessciprt + Node.js + Express");
 });
 
 app.post("/user/login", (req: Request, res: Response) => {
-  console.log(req.body);
   const { username, password }: { username: string; password: string } =
     req.body;
   if (username.length > 1 && username === password) {
-    res.status(200).json({ accessToken: "" }).send();
+    const refreshToken = jwt.sign({ username }, secretKey, {
+      expiresIn: "1h",
+    });
+    res
+      .cookie("refreshToken", refreshToken, { httpOnly: true })
+      .status(200)
+      .json({ message: "Login success" })
+      .send();
+  } else {
+    res.status(401).json({ message: "username and password mismatch" }).send();
   }
-  res.status(401).send();
+});
+
+app.get("/user/accessToken", (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+  if (!refreshToken) {
+    res.status(401).json({ message: "RefreshToken not found" }).send();
+  }
+  try {
+    const decoded = jwt.verify(refreshToken, secretKey);
+    const { username } = decoded as { username: string };
+    res.status(200).json({ username }).send();
+  } catch (err) {
+    res.status(401).json({ message: "RefreshToken cannot verified" }).send();
+  }
 });
 
 const port = 5001;
